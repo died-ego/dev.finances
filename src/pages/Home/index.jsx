e81a9966c64bfdd759429aed8e861e690c6bae02
died-ego <diegofreitas.oliveira01@gmail.com>
2021-01-15 14:04:55 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <div id="page-home">
+      <h1>Dale</h1>
+    </div>
+  );
+};
+
+export default Home;
