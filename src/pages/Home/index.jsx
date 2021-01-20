86bcfaf5e9c8102a91ed793da5f277e0935965b0
died-ego <diegofreitas.oliveira01@gmail.com>
2021-01-20 14:05:37 -0300

    first-class

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,67 @@ import "./styles.css";
 const Home = () => {
   return (
     <div id="page-home">
-      <h1>Dale</h1>
+      <header>
+        <h1 id="logo">dev.finance$</h1>
+      </header>
+
+      <main className="container">
+        <section id="balance">
+          <h2>Balanço</h2>
+
+          <div className="card">
+            <h3>Entradas</h3>
+            <p>R$ 5.000,00</p>
+          </div>
+
+          <div className="card">
+            <h3>Saídas</h3>
+            <p>R$ 2.000,00</p>
+          </div>
+
+          <div className="card total">
+            <h3>Total</h3>
+            <p>R$ 3.000,00</p>
+          </div>
+        </section>
+
+        <section id="transaction">
+          <h2>Transações</h2>
+          <table id="transactions-table">
+            <thead>
+              <tr>
+                <th>Descrição</th>
+                <th>Valor</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td className="description">Luz</td>
+                <td className="expense">- R$ 500,00</td>
+                <td className="date">23/01/2021</td>
+              </tr>
+
+              <tr>
+                <td className="description">Criação de Website</td>
+                <td className="income">+ R$5.000,00</td>
+                <td className="date">29/01/2021</td>
+              </tr>
+
+              <tr>
+                <td className="description">Upgrade do PC</td>
+                <td className="expense">- R$1.500,00</td>
+                <td className="date">05/01/2021</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </main>
+
+      <footer>
+        <p>dev.finance$</p>
+      </footer>
     </div>
   );
 };
